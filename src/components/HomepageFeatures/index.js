e99afc9bc812a4b86b29bdfc08f7936cfc15525c
AKoreanDumpling <bookworm8141@gmail.com>
2025-03-23 17:28:03 -0700

    Installed anyl?

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,37 +3,37 @@ import clsx from "clsx";
 import styles from "./styles.module.css";
 
 const FeatureList = [
-  {
-    title: "Special Thanks:",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-  },
+	{
+		title: "Special Thanks:",
+		Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+	},
 ];
 
 function Feature({ Svg, title, description }) {
-  return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <h3>{title}</h3>
-        <Svg className={styles.featureSvg} role="img" />
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={clsx("col col--4")}>
+			<div className="text--center">
+				<h3>{title}</h3>
+				<Svg className={styles.featureSvg} role="img" />
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
 }
